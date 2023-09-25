973e8867d783f77e08fcbe645e0980af7bd428ff
Nadezhda Ugay <777ugay@gmail.com>
2023-09-25 20:30:12 +0300

    - CRUD for Product - Model for Version of Product - inline form for updating Product

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category, Contacts
+from catalog.models import Product, Category, Contacts, Version
 
 
 @admin.register(Product)
@@ -8,6 +8,13 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'price', 'category', 'created_at', 'updated_at', 'pic')
     list_filter = ('category',)
     search_fields = ('title', 'description',)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'num', 'title', 'product', 'is_active', 'created_at', 'updated_at',)
+    list_filter = ('product', 'is_active')
+    search_fields = ('title',)
 
 
 @admin.register(Category)
